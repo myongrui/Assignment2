df4388636a45962656bca5a695d4b6a4c7b25112
myongrui <maryongrui@gmail.com>
2022-02-06 15:47:32 +0800

    Added WorldWide cases graph

---
+++ indexscript.js
@@ -0,0 +1,77 @@
+
+const api = 'https://corona.lmao.ninja/v2/historical/all?lastdays=365';
+
+document.addEventListener("DOMContentLoaded", function(event) {
+fetch(api)
+    .then(function(response) { return response.json(); })
+    .then(function(data) {
+        var parsedData = parseData(data);
+        drawChart(parsedData);
+         
+    })
+    .catch(function(err) { console.log(err); })
+});
+
+function parseData(data) {
+    var arr = [];
+    for (var i in data.deaths) {
+        arr.push({
+            date: new Date(i), 
+            value: +data.deaths [i] 
+        });
+    }
+    return arr;
+}
+
+function drawChart(data) {
+var svgWidth = 750, svgHeight = 750;
+var margin = { top: 30, right: 20, bottom: 30, left: 100 };
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+var svg = d3.select('.cases')
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+    
+var g = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var x = d3.scaleTime()
+    .rangeRound([0, width]);
+
+var y = d3.scaleLinear()
+    .rangeRound([height, 0]);
+
+var line = d3.line()
+    .x(function(d) { return x(d.date)})
+    .y(function(d) { return y(d.value)})
+    x.domain(d3.extent(data, function(d) { return d.date }));
+    y.domain(d3.extent(data, function(d) { return d.value }));
+
+g.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .select(".domain")
+    .remove();
+
+g.append("g")
+    .call(d3.axisLeft(y))
+    .append("text")
+    .attr("fill", "#000")
+    .attr("transform", "")
+    .attr("y", 10)
+    .attr("dy", "-20px")
+    .attr("text-anchor", "end")
+    .text("Cases")
+    .style("font-size", "15px")
+
+g.append("path")
+    .datum(data)
+    .attr("fill", "none")
+    .attr("stroke", "red")
+    .attr("stroke-linejoin", "round")
+    .attr("stroke-linecap", "round")
+    .attr("stroke-width", 1.5)
+    .attr("d", line);
+}
+
