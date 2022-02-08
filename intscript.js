470f02371c3cc9f94c4dc353f4ae02385f3de185
myongrui <maryongrui@gmail.com>
2022-02-08 16:05:41 +0800

    added worldwide map and interaction

---
+++ intscript.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var au = $("svg path")
+    console.log(au[0])
+    console.log(au[1])
+    var au0 = au[0]
+    au0.addEventListener("click", function(){
+        console.log("yes");
+    })
+})
