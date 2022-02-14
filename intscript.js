702c9151d9939fa378a776f895bf711bff7df339
myongrui <maryongrui@gmail.com>
2022-02-14 20:12:53 +0800

    heatmap for international cases

--- intscript.js
+++ intscript.js
@@ -4,16 +4,35 @@ document.addEventListener("DOMContentLoaded", function(){
     for (let i = 0; i < map.length; i++){
         let c = map[i]
         let n = c.className.baseVal
-        console.log(n)
         let covidapi = "https://corona.lmao.ninja/v2/countries/"+n+"?strict=true"
         fetch(covidapi)
             .then(response => response.json())
             .then(function(data){
                 let cpm = data.casesPerOneMillion;
+                console.log(n)
+                console.log(cpm)
+                let cn = (n.split(" "))[0] 
+                console.log(cn)
                 if (cpm >= 0 && cpm < 100){
-                    $("svg ."+n).css({fill: "pink"})
-                    console.log("yes");
+                    $("svg ."+cn).css({fill: "#fff7fb"})
+                }   else if (cpm >= 100 && cpm < 500){
+                    $("svg ."+cn).css({fill: "#ece7f2"})
+                }   else if (cpm >= 500 && cpm < 1000){
+                    $("svg ."+cn).css({fill: "#d0d1e6"})    
+                }   else if (cpm >= 1000 && cpm < 5000){
+                    $("svg ."+cn).css({fill: "#a6bddb"})
+                }   else if (cpm >= 5000 && cpm < 10000){
+                    $("svg ."+cn).css({fill: "#74a9cf"})
+                }   else if (cpm >= 10000 && cpm < 50000){
+                    $("svg ."+cn).css({fill: "#3690c0"})
+                }   else if (cpm >= 50000 && cpm < 100000){
+                    $("svg ."+cn).css({fill: "#0570b0"})
+                }   else if (cpm >= 100000 && cpm < 500000){
+                    $("svg ."+cn).css({fill: "#045a8d"})
+                }   else if (cpm >= 500000 && cpm < 1000000){
+                    $("svg ."+cn).css({fill: "#023858"})
                 }
+                
             })
     }   
 })
