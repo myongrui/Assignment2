13dac2950e1d0c270cc86ecc565f85e51b94822e
casxavier <s10223292@connect.np.edu.sg>
2022-02-15 16:14:19 +0800

    added in quiz page

--- quizscript.js
+++ quizscript.js
@@ -2,41 +2,125 @@ const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
 
-function buildQuiz(){}
+function buildQuiz(){
+    // variable to store output
+    const output = [];
+  
+    // for each question...
+    myQuestions.forEach(
+      (currentQuestion, questionNumber) => {
+  
+        const answers = [];
+  
+        for(letter in currentQuestion.answers){
+  
+          // add radio button
+          answers.push(
+            `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+          );
+        }
+  
+        output.push(
+          `<div class="question"> ${currentQuestion.question} </div>
+          <div class="answers"> ${answers.join('')} </div>`
+        );
+      }
+    );
+  
+    quizContainer.innerHTML = output.join('');
+  }
 
-function showResults(){}
 
-buildQuiz();
+function showResults(){
 
-submitButton.addEventListener('click', showResults);
+    // gather answer containers
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+  
+    // keep track of answers
+    let numCorrect = 0;
+  
+    myQuestions.forEach( (currentQuestion, questionNumber) => {
+  
+      // find selected answer
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+  
+      // checking if answer is correct
+      if(userAnswer === currentQuestion.correctAnswer){
+        numCorrect++;
+  
+        answerContainers[questionNumber].style.color = 'green';
+      }
+
+      // wrong/blank ans
+      else{
+        answerContainers[questionNumber].style.color = 'red';
+      }
+    });
+  
+    // show number of correct answers
+    resultsContainer.innerHTML = "<b>Score: </b>"+ `<b>${numCorrect} / ${myQuestions.length}</b>`;
+  }
 
 const myQuestions = [
     {
-      question: "Who invented JavaScript?",
+      question: "Which of the following are false symptoms of Covid-19?",
       answers: {
-        a: "Douglas Crockford",
-        b: "Sheryl Sandberg",
-        c: "Brendan Eich"
+        a: "Fever",
+        b: "Cough",
+        c: "Poor concentration",
+        d: "Lost of taste/smell",
+        e: "Loss of speech/mobility or confusion"
       },
       correctAnswer: "c"
     },
     {
-      question: "Which one of these is a JavaScript package manager?",
+      question: "Can the Covid-19 vaccines cause you to become magnetic?",
       answers: {
-        a: "Node.js",
-        b: "TypeScript",
-        c: "npm"
+        a: "True",
+        b: "False",
       },
-      correctAnswer: "c"
+      correctAnswer: "b"
     },
     {
-      question: "Which tool can you use to ensure code quality?",
+      question: "Who can contract Covid-19?",
       answers: {
-        a: "Angular",
-        b: "jQuery",
-        c: "RequireJS",
-        d: "ESLint"
+        a: "Everyone",
+        b: "Chidren",
+        c: "Elderly",
+        d: "Adults"
       },
-      correctAnswer: "d"
+      correctAnswer: "a"
+    },
+    {
+        question: "Can home remedies (vitamin C, essential oils, fish tank cleaner, bleach etc.) cure and protect against Covid-19?",
+        answers: {
+          a: "Yes, defintely",
+          b: "No, these do not protect and cure you against Covid-19 even if you sip it every 15 minutes.",
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Did Covid-19 start beacause people drank bat soup?",
+        answers: {
+          a: "Yes, obviously",
+          b: "No, despite research showing that the virus may have started in animals, there is no evidence that soup was involved. ",
+        },
+        correctAnswer: "b"
     }
   ];
+
+  
+  
+//start and display quiz
+buildQuiz();
+submitButton.addEventListener('click', showResults);
+
+function ani() {
+    document.getElementById('submit');
+  }
