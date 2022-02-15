03001522f2fdfbd4f388d1bf57d60f4e444c63f9
myongrui <maryongrui@gmail.com>
2022-02-15 23:35:18 +0800

    updated international and font

--- intscript.js
+++ intscript.js
@@ -1,9 +1,12 @@
+let allcountries = new Set();
+
 document.addEventListener("DOMContentLoaded", function(){
     var map = $("svg path")
     console.log($("svg .Bosnia"))
     for (let i = 0; i < map.length; i++){
         let c = map[i]
         let n = c.className.baseVal
+        allcountries.add(n)
         let covidapi = "https://corona.lmao.ninja/v2/countries/"+n+"?strict=true"
         fetch(covidapi)
             .then(response => response.json())
@@ -33,8 +36,33 @@ document.addEventListener("DOMContentLoaded", function(){
                     $("svg ."+cn).css({fill: "#023858"})
                 }
                 
-            })
-    }   
+    })
+
+
+    }
+    let covidapi ='https://disease.sh/v3/covid-19/all';
+
+    fetch(covidapi)
+    .then(response => response.json())
+    .then(function(data){
+        console.log(data)
+        let cases = data.cases
+        let deaths = data.deaths
+        let recovered = data.recovered
+        let newc = data.todayCases
+        let newd = data.todayDeaths
+        let newr = data.todayRecovered
+        $('.cname').text("World")
+        $('p.livecase').text("Total Cases")
+        $('p.livecase').append("<br /><strong>" + cases + "</strong>")
+        $('p.livecase').append("<br /><i>+" + newc + "</i>")
+        $('p.livedeaths').text("Total Deaths")
+        $('p.livedeaths').append("<br /><strong>" + deaths + "</strong>")
+        $('p.livedeaths').append("<br /><i>+" + newd + "</i>")
+        $('p.liverecovered').text("Recovered")
+        $('p.liverecovered').append("<br /><strong>" + recovered + "</strong>")
+        $('p.liverecovered').append("<br /><em>+" + newr + "</em>")
+    })   
 })
 
 $(document).click(function(event){
@@ -65,10 +93,97 @@ $(document).click(function(event){
                     $('p.liverecovered').text("Recovered")
                     $('p.liverecovered').append("<br /><strong>" + recovered + "</strong>")
                     $('p.liverecovered').append("<br /><em>+" + newr + "</em>")
+                    const api = 'https://corona.lmao.ninja/v2/historical/'+countryname+'?lastdays=365';
+
+                    fetch(api)
+                        .then(function(response) { return response.json(); })
+                        .then(function(data) {
+                            var parsedData = parseData(data);
+                            drawChart(parsedData);
+                            
+                        })
+                        .catch(function(err) { console.log(err); })
+
+                    function parseData(data) {
+                        var arr = [];
+                        console.log(data.timeline.cases)
+                        for (var i in data.timeline.cases) {
+                            arr.push({
+                                date: new Date(i), 
+                                value: +data.timeline.cases [i] 
+                            });
+                        }
+                        return arr;
+                    }
+
+                    function drawChart(data) {
+                    var svgWidth = 750, svgHeight = 750;
+                    var margin = { top: 30, right: 20, bottom: 30, left: 100 };
+                    var width = svgWidth - margin.left - margin.right;
+                    var height = svgHeight - margin.top - margin.bottom;
+
+                    var svg = d3.select('.cases')
+                        .attr("width", svgWidth)
+                        .attr("height", svgHeight);
+                        
+                    var g = svg.append("g")
+                        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+                    var x = d3.scaleTime()
+                        .rangeRound([0, width]);
+
+                    var y = d3.scaleLinear()
+                        .rangeRound([height, 0]);
+
+                    var line = d3.line()
+                        .x(function(d) { return x(d.date)})
+                        .y(function(d) { return y(d.value)})
+                        x.domain(d3.extent(data, function(d) { return d.date }));
+                        y.domain(d3.extent(data, function(d) { return d.value }));
+
+                    g.append("g")
+                        .attr("transform", "translate(0," + height + ")")
+                        .call(d3.axisBottom(x))
+                        .select(".domain")
+                        .remove();
+
+                    g.append("g")
+                        .call(d3.axisLeft(y))
+                        .append("text")
+                        .attr("fill", "#000")
+                        .attr("transform", "")
+                        .attr("y", 10)
+                        .attr("dy", "-20px")
+                        .attr("text-anchor", "end")
+                        .text("Cases")
+                        .style("font-size", "15px")
+
+                    g.append("path")
+                        .datum(data)
+                        .attr("fill", "none")
+                        .attr("stroke", "red")
+                        .attr("stroke-linejoin", "round")
+                        .attr("stroke-linecap", "round")
+                        .attr("stroke-width", 1.5)
+                        .attr("d", line);
+                    }
+
                 }          
             })
+        
     }
 })
+
+
+
+$("button").click(function(event){
+    console.log("yes");  
+    console.log(allcountries);  
+})
+
+
+
+
 
 
 
